dfe41930d6a548310570c5b67772fc7fee8f4d86
charly <charlyreux@gmail.com>
2022-08-04 20:51:33 +0200

    Testing download readmes

--- download_script/readmeDL.py
+++ download_script/readmeDL.py
@@ -17,5 +17,5 @@ for rep in JsonRepos:
     RHtml = requests.get("https://api.github.com/repos/CharlyReux/"+rep["name"]+"/readme",headers={"Accept": "application/vnd.github.html","Authorization":sys.argv[0]})
     
 
-    puReq = requests.put("https://api.github.com/repos/CharlyReux/myWebsite/contents/Readmes/"+rep["name"]+"_readme",data={"message":"getting Readme","committer":{"name":"githubRunner","email":"charlyreux@gmail.com"},"content":base64.b64encode(RHtml.content)},headers={"Accept": "application/vnd.github.html","Authorization":sys.argv[0]})
+    puReq = requests.put("https://api.github.com/repos/CharlyReux/myWebsite/contents/Readmes/"+rep["name"]+"_readme.html",data={"message":"getting Readme","committer":{"name":"CharlyReux","email":"charlyreux@gmail.com"},"content":base64.b64encode(RHtml.content)},headers={"Accept": "application/vnd.github.html","Authorization":sys.argv[0]})
     print(puReq.content)
