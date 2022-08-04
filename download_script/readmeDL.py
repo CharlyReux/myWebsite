53d7315b98ee929cce84acd88e4be4b7b25bf05a
charly <charlyreux@gmail.com>
2022-08-04 20:58:28 +0200

    Testing download readmes

--- download_script/readmeDL.py
+++ download_script/readmeDL.py
@@ -8,14 +8,16 @@ import json
 import sys
 import base64
 
-AllRepos= requests.get("https://api.github.com/users/CharlyReux/repos",headers={"Accept":"application/vnd.github+json","Authorization":"token " +sys.argv[0]})
+token = sys.argv[0]
+
+AllRepos= requests.get("https://api.github.com/users/CharlyReux/repos",headers={"Accept":"application/vnd.github+json","Authorization":"token "+token})
 
 JsonRepos = json.loads(AllRepos.content)
 
 for rep in JsonRepos:
     
-    RHtml = requests.get("https://api.github.com/repos/CharlyReux/"+rep["name"]+"/readme",headers={"Accept": "application/vnd.github.html","Authorization":"token " +sys.argv[0]})
+    RHtml = requests.get("https://api.github.com/repos/CharlyReux/"+rep["name"]+"/readme",headers={"Accept": "application/vnd.github.html","Authorization":"token "+token})
     
 
-    puReq = requests.put("https://api.github.com/repos/CharlyReux/myWebsite/contents/Readmes/"+rep["name"]+"_readme.html",data={"message":"getting Readme","committer":{"name":"CharlyReux","email":"charlyreux@gmail.com"},"content":base64.b64encode(RHtml.content)},headers={"Accept": "application/vnd.github.html","Authorization":"token " +sys.argv[0]})
+    puReq = requests.put("https://api.github.com/repos/CharlyReux/myWebsite/contents/Readmes/"+rep["name"]+"_readme.html",data={"message":"getting Readme","committer":{"name":"CharlyReux","email":"charlyreux@gmail.com"},"content":base64.b64encode(RHtml.content)},headers={"Accept": "application/vnd.github.html","Authorization":"token "+token})
     print(puReq.content)
